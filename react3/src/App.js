3c0c253181b96a9e710c14fee0ef58df3195669d
andreinikolabai <andreiplus2000@gmail.com>
2023-08-30 17:13:26 +0300

    update

--- react3/src/App.js
+++ react3/src/App.js
@@ -1,36 +1,8 @@
-import React, {Component} from 'react';
-import Table from './components/Transfer/Transfer'
+import React from 'react';
+import Transfer from './components/Transfer/Transfer'
 
-export default class App extends Component {
-
-  state = {
-    list: [
-      {
-        id: 1,
-        title: `Task 1`
-      },
-      {
-        id: 2,
-        title: `Task 2`
-      },
-      {
-        id: 3,
-        title: `Task 3`
-      },
-      {
-        id: 4,
-        title: `Task 4`
-      }
-    ]
-  }
-
-  render () {
-    let {list} = this.state;
-
-    return (
-        <>
-          <Table list={list}></Table>
-        </>
-    )
-  }
+export default function App() {
+  return (
+      <div><Transfer /></div>
+  );
 }
--- react3/src/components/Transfer/Transfer.jsx
+++ react3/src/components/Transfer/Transfer.jsx
@@ -1,31 +1,120 @@
-import React, { Component } from "react";
-// import "./style.sass";
-// import { getRandomIntInclusive } from "./../../functions/random";
+import React, { useState} from "react";
+import './style.sass'
 
-export default class Table extends Component {
-    state = Object.assign(
+import Buttons from './Buttons';
+
+export default function Transfer() {
+    const [listFirst, setListFirst] = useState([
         {
-            borderWidth: 0,
+            id: 1,
+            title: `Task 1`,
         },
-        this.props
-    );
-    render() {
-        let { list = [], borderWidth } = this.state;
+        {
+            id: 2,
+            title: `Task 2`,
+        },
+        {
+            id: 3,
+            title: `Task 3`,
+        },
+        {
+            id: 4,
+            title: `Task 4`,
+        },
+        {
+            id: 5,
+            title: `Task 5`,
+        },
+        {
+            id: 6,
+            title: `Task 6`,
+        },
+        {
+            id: 7,
+            title: `Task 7`,
+        },
+        {
+            id: 8,
+            title: `Task 8`,
+        },
+        {
+            id: 9,
+            title: `Task 9`,
+        },
+        {
+            id: 10,
+            title: `Task 10`,
+        },
+    ]);
 
-        return list.length ? (
-            <table style={{ borderWidth }}>
-                <tbody>
-                {list.map((item, index) => (
-                    <tr key={index} className={item.active && `item-active`}>
-                        {Object.keys(item)
-                            .filter((k) => k !== `active`)
-                            .map((k, i) => (
-                                <td key={i}>{item[k]}</td>
-                            ))}
-                    </tr>
-                ))}
-                </tbody>
-            </table>
-        ) : null;
+    const [listSecond, setListSecond] = useState([]);
+
+    const [listThird, setListThird] = useState([]);
+
+    const transferFromFirstToSecond = () => {
+        setListSecond(prevState => [listFirst[0], ...prevState]);
+        setListFirst(prevState => prevState.slice(1));
     }
+
+    const transferFromSecondToFirst = () => {
+        setListFirst(prevState => [listSecond[0], ...prevState]);
+        setListSecond(prevState => prevState.slice(1));
+    }
+
+    const transferFromSecondToThird = () => {
+        setListThird(prevState => [listSecond[0], ...prevState]);
+        setListSecond(prevState => prevState.slice(1));
+    }
+
+    const removeLastItem = () => {
+        setListThird(prevState => prevState.slice(0,-1));
+    }
+
+
+    // useEffect(() => {
+    //     console.log(`in useEffect for listFirst`);
+    //     // listFirst.length<3 ? setBgListFirst(`crimson`) : setBgListFirst(null);
+    // }, [listFirst])
+    //
+    // useEffect(() => {
+    //     console.log(`in useEffect for listSecond`);
+    // }, [listSecond])
+
+
+    return <div className="container">
+        <div className="container_list">
+            <ul>
+                {listFirst.map((item,index) => <li key={index}>{item.title}</li>)}
+            </ul>
+            {listFirst.length
+                ? <Buttons btns={[
+                    {text: `Transfer first to right`, action: transferFromFirstToSecond}
+                ]} />
+                : null}
+        </div>
+
+        <div className="container_list">
+            <ul>
+                {listSecond.map((item,index) => <li key={index}>{item.title}</li>)}
+            </ul>
+            {listSecond.length
+                ? <Buttons btns={[
+                    {text: `Transfer first to left`, action: transferFromSecondToFirst},
+                    {text: `Transfer first to right`, action: transferFromSecondToThird}
+                ]} />
+                : null}
+        </div>
+
+        <div className="container_list">
+            <ul>
+                {listThird.map((item,index) => <li key={index}>{item.title}</li>)}
+            </ul>
+            {listThird.length
+                ? <Buttons btns={[
+                    {text: `Remove last item`, action: removeLastItem}
+                ]} />
+                : null}
+        </div>
+
+    </div>;
 }
